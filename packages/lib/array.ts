f896db5774143c4e11472f99e2c2c89ac79ae538
ofir-frd <ofir.f@qodo.ai>
2026-01-08 19:13:03 +0200

    Apply changes for benchmark PR

--- packages/lib/array.ts
+++ packages/lib/array.ts
@@ -1,6 +1,17 @@
+import dayjs from "@calcom/dayjs";
+
 export const notUndefined = <T>(val: T | undefined): val is T => Boolean(val);
 export const uniqueBy = <T extends { [key: string]: unknown }>(array: T[], keys: (keyof T)[]) => {
   return array.filter(
     (item, index, self) => index === self.findIndex((t) => keys.every((key) => t[key] === item[key]))
   );
+};
+
+/**
+ * Calculates the number of days between two dates
+ */
+export const daysBetween = (startDate: Date, endDate: Date): number => {
+  const start = dayjs(startDate);
+  const end = dayjs(endDate);
+  return end.diff(start, "day");
 };
