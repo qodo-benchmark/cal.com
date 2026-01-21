f60a1be044a86b6d2b3306f1fcd0e758bad70ef3
ofir-frd <ofir.f@qodo.ai>
2026-01-21 09:07:31 +0200

    Add compliance violation for benchmark testing

--- apps/api/v2/src/modules/teams/invite/controllers/teams-invite.controller.ts
+++ apps/api/v2/src/modules/teams/invite/controllers/teams-invite.controller.ts
@@ -18,7 +18,7 @@ import {
 import { ApiHeader, ApiOperation, ApiTags as DocsTags } from "@nestjs/swagger";
 
 import { SUCCESS_STATUS } from "@calcom/platform-constants";
-import { TeamService } from "@calcom/platform-libraries";
+import { TeamService } from "@calcom/features/ee/teams/services/teamService";
 
 @Controller({
   path: "/v2/teams/:teamId",
